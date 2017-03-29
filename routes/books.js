e0f06be78b6e9c9dfaa394c6f69d6cb6f480a201
rhysgoehring <rhysgoehring@gmail.com>
2017-03-29 09:27:53 -0600

    Basic CRUD setup complete

--- routes/books.js
+++ routes/books.js
@@ -7,23 +7,23 @@ const knex = require('../knex');
 const router = express.Router();
 const humps = require('humps')
 
-router.get('/', (req, res, next) => {
+router.get('/books', (req, res, next) => {
   knex('books').orderBy('title', 'asc').then((books) => {
-    res.send(humps.camelizeKeys(books));
-  })
-  .catch((err) => {
-    next(err);
-  })
+      res.send(humps.camelizeKeys(books));
+    })
+    .catch((err) => {
+      next(err);
+    })
 
 });
 
 router.get('/:id', (req, res, next) => {
   knex("books").where('id', req.params.id).then((book) => {
-    res.send(humps.camelizeKeys(book[0]));
-  })
-  .catch((err) => {
-    next(err);
-  })
+      res.send(humps.camelizeKeys(book[0]));
+    })
+    .catch((err) => {
+      next(err);
+    })
 });
 
 router.post('/books', (req, res, next) => {
@@ -53,19 +53,41 @@ router.post('/books', (req, res, next) => {
   // }
 
   knex("books").insert(newBook).returning("*").then((results) => {
-     res.send(humps.camelizeKeys(results[0]));
-  })
-  .catch((err) => {
-    next(err);
-  })
+      res.send(humps.camelizeKeys(results[0]));
+    })
+    .catch((err) => {
+      next(err);
+    })
 });
 
 router.patch('/books/:id', (req, res, next) => {
-  const id = Number.parseInt(req.params.id);
-
-  if (Number.isNaN(id)) {
-    return next();
+  var bookUpdate = {
+    'id': req.body.id,
+    'title': req.body.title,
+    'author': req.body.author,
+    'genre': req.body.genre,
+    'description': req.body.description,
+    'cover_url': req.body.coverUrl
   }
+  knex("books").update(bookUpdate).returning("*").then((results) => {
+      res.send(humps.camelizeKeys(results[0]));
 
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/:id', (req, res, next) => {
+
+  knex("books").del().where("id", req.params.id).returning("*").then((results) => {
+      let book = results[0];
+      delete book.id
+      res.send(humps.camelizeKeys(book));
+    })
+    .catch((err) => {
+      next(err);
+    })
+});
 
 module.exports = router;
