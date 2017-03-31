c5acdf984eab72de3f91cdc7cc8c967028d0862e
rhysgoehring <rhysgoehring@gmail.com>
2017-03-31 09:41:40 -0600

    part 3 complete

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,41 @@
 'use strict';
 
 const express = require('express');
-
+const app = express();
+const knex = require('../knex');
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const humps = require('humps');
+const bcrypt = require('bcrypt');
 
-// YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+// bcrypt.hash(req.body.password, 12)
+//   .then((hashed_password) => {
+knex("users").returning(["id", "first_name", "last_name", "email"]).insert({"first_name": req.body.firstName,
+  "last_name": req.body.lastName,
+  "email": req.body.email,
+  "hashed_password": bcrypt.hashSync(req.body.password, 8)
+}).then((user) => {
+  res.send(humps.camelizeKeys(user[0]))
+});
+
+
+
+// var newUsers = {
+//   'id': req.body.id,
+//   'first_name': req.body.firstName,
+//   'last_name': req.body.lastName,
+//   'email': req.body.email,
+//   'hashed_password': bcrypt.hash(req.body.password, 8)
+// };
+
+// knex("users").insert(newUsers).returning("*").then((results) => {
+//     res.send(humps.camelizeKeys(results[0]));
+//   })
+//   .catch((err) => {
+//     next(err);
+//   })
+});
+
 
 module.exports = router;
--- seeds/2_users.js
+++ seeds/2_users.js
@@ -1,13 +1,17 @@
-
-exports.seed = function(knex, Promise) {
+exports.seed = (knex) => {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
-      ]);
-    });
+  return knex('users').del()
+    .then(() => {
+      return knex('users').insert([{
+  id: 1,
+  first_name: 'Joanne',
+  last_name: 'Rowling',
+  email: 'jkrowling@gmail.com',
+  hashed_password: '$2a$12$C9AYYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',  // youreawizard
+  created_at: new Date('2016-06-29 14:26:16 UTC'),
+  updated_at: new Date('2016-06-29 14:26:16 UTC')
+}]);
+  }).then(() => {
+    return knex.raw("select setval('users_id_seq', (select max(id) from users));")
+  })
 };
