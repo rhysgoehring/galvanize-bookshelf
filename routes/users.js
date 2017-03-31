297c37d844b9100cfd630e0dcfe90da0ff35ace8
rhysgoehring <rhysgoehring@gmail.com>
2017-03-31 09:43:39 -0600

    cleaned up users.js

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,6 @@ const humps = require('humps');
 const bcrypt = require('bcrypt');
 
 router.post('/users', (req, res, next) => {
-// bcrypt.hash(req.body.password, 12)
-//   .then((hashed_password) => {
 knex("users").returning(["id", "first_name", "last_name", "email"]).insert({"first_name": req.body.firstName,
   "last_name": req.body.lastName,
   "email": req.body.email,
@@ -19,22 +17,6 @@ knex("users").returning(["id", "first_name", "last_name", "email"]).insert({"fir
   res.send(humps.camelizeKeys(user[0]))
 });
 
-
-
-// var newUsers = {
-//   'id': req.body.id,
-//   'first_name': req.body.firstName,
-//   'last_name': req.body.lastName,
-//   'email': req.body.email,
-//   'hashed_password': bcrypt.hash(req.body.password, 8)
-// };
-
-// knex("users").insert(newUsers).returning("*").then((results) => {
-//     res.send(humps.camelizeKeys(results[0]));
-//   })
-//   .catch((err) => {
-//     next(err);
-//   })
 });
 
 
